770748a0ea05d11ebae56828bd9f267209be8496
Sentry2703 <adetunjiadewoye1@gmail.com>
2023-11-06 00:12:56 -0500

    Code to enable the drop down

--- src/app.js
+++ src/app.js
@@ -3,3 +3,20 @@ fetch("header.html")
 .then(headerContent => {
     document.getElementById("header").innerHTML = headerContent;
 });
+
+function expandFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
